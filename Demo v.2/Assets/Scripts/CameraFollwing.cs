cbe6d92f9c488678f21ea4557976b10dfe3b4095
ThatZombieAlien <sanna.gustafsson0706@gmail.com>
2016-03-31 11:45:08 +0200

    Demo v.2
    Lade till nytt projekt - Demo v.2. Fortsätt implementera i detta projekt
    istället!

---
+++ Demo v.2/Assets/Scripts/CameraFollwing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollwing : MonoBehaviour {
+
+    public Transform target;
+    Camera cam;
+    float zoomOut = 80f; // mindre värde = större ut zoomning
+	void Start () 
+    {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        cam.orthographicSize = (Screen.height / zoomOut) / 4f;  
+
+        if(target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.5f) + new Vector3(0, 0, -10);
+            // Lerp: (från, till, fart)
+        }
+	}
+}
---
+++ Demo v.2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rigidBody;
+    Animator anim;
+
+    public float moveSpeed;
+    private float currentMoveSpeed;
+    public float diagonalSpeedModifier;
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
+        if (movementVector != Vector2.zero)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("inputX", movementVector.x);
+            anim.SetFloat("inputY", movementVector.y);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        rigidBody.MovePosition(rigidBody.position + movementVector * Time.deltaTime);
+
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+        {
+            currentMoveSpeed = moveSpeed * diagonalSpeedModifier;
+        }
+        else
+        {
+            currentMoveSpeed = moveSpeed;
+        }
+
+    }
+
+}
