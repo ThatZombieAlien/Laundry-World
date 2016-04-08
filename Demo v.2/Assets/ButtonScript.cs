5a0240e39d3328730d0418b2f9193739e3c8f41b
annaliljaenglund <annaliljaenglund@gmail.com>
2016-04-08 12:35:04 +0200

    Inventory - uppdatering
    Har lagt till en ryggsäck; om man klickar på den kommer inventoryn upp,
    och trycker man på Q-tangenten stängs den ned.

---
+++ Demo v.2/Assets/ButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour
+{
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    GameObject textPanel;
+    GameObject textText;
+    GameObject backpack;
+
+    void Start()
+    {
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        textPanel = GameObject.Find("Title Panel");
+        textText = textPanel.transform.FindChild("Title").gameObject;
+        backpack = GameObject.Find("Backpack");
+    }
+
+    public void OnBackPackClick()
+    {
+        inventoryPanel.SetActive(true);
+        slotPanel.SetActive(true);
+        textPanel.SetActive(true);
+        textText.SetActive(true);
+
+        backpack.SetActive(false);
+    }
+}
