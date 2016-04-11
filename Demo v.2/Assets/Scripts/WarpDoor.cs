0b8db918e8cf68e4d1b7d0c1730c6a4e1be52617
ThatZombieAlien <sanna.gustafsson0706@gmail.com>
2016-04-11 12:32:31 +0200

    Kollisioner och Warpning
    Lagt in kollisioner, organiserat lager, och lagt in alla warping
    kollisioner.

---
+++ Demo v.2/Assets/Scripts/WarpDoor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpDoor : MonoBehaviour
+{
+
+    public Transform warpTarget;
+    public AudioSource doorSound;
+
+
+    IEnumerator OnTriggerEnter2D(Collider2D other)
+    {
+        ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+
+        doorSound.Play();
+
+        yield return StartCoroutine(sf.FadeToBlack());
+
+        Debug.Log("An object Collided");
+
+        other.gameObject.transform.position = warpTarget.position;
+        Camera.main.transform.position = warpTarget.position;
+
+        yield return StartCoroutine(sf.FadeToClear());
+    }
+}
