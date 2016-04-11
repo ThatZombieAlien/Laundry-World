076b35318068a804c733c26ba6eee90b8c4dd27c
annaliljaenglund <annaliljaenglund@gmail.com>
2016-04-11 16:55:44 +0200

    Inventory - ändring 3
    Fixat till inventoryn

---
+++ Demo v.2/Assets/Scripts/BackpackButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackpackButtonScript : MonoBehaviour
+{
+    Inventory inventory;
+
+    void Start()
+    {
+        //inventoryPanel = GameObject.Find("Inventory Panel");
+        //slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        //textPanel = GameObject.Find("Title Panel");
+        //textText = textPanel.transform.FindChild("Title").gameObject;
+        //backpack = GameObject.Find("Backpack");
+
+        inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
+
+        inventory.inventoryPanel = GameObject.Find("Inventory Panel");
+        inventory.slotPanel = inventory.inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        inventory.textPanel = GameObject.Find("Title Panel");
+        inventory.textText = inventory.textPanel.transform.FindChild("Title").gameObject;
+        inventory.backpack = GameObject.Find("Backpack");
+    }
+
+    public void OnBackPackClick()
+    {
+        inventory.inventoryPanel.SetActive(true);
+        inventory.slotPanel.SetActive(true);
+        inventory.textPanel.SetActive(true);
+        inventory.textText.SetActive(true);
+
+        inventory.backpack.SetActive(false);
+    }
+}
