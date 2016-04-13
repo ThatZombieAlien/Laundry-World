e18654d01617c1017306456be9d07789ab139638
annaliljaenglund <annaliljaenglund@gmail.com>
2016-04-13 10:02:27 +0200

    Map border - kollision

--- Demo v.2/Assets/Scripts/CameraFollwing.cs
+++ Demo v.2/Assets/Scripts/CameraFollwing.cs
@@ -6,6 +6,11 @@ public class CameraFollwing : MonoBehaviour {
     public Transform target;
     Camera cam;
     float zoomOut = 80f; // mindre värde = större ut zoomning
+
+    public bool bounds;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
 	void Start () 
     {
         cam = GetComponent<Camera>();
@@ -21,5 +26,13 @@ public class CameraFollwing : MonoBehaviour {
             transform.position = Vector3.Lerp(transform.position, target.position, 0.5f) + new Vector3(0, 0, -10);
             // Lerp: (från, till, fart)
         }
+
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+
 	}
 }
