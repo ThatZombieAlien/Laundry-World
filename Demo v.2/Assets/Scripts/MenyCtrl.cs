aaf2c0e5ff8ea1de7ac47dff28136a2e89a0d90c
MrMagicSticK <INTHEMOOD74@gmail.com>
2016-04-14 00:46:54 +0200

    dave
    heatlhbar , health meny

---
+++ Demo v.2/Assets/Scripts/MenyCtrl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenyCtrl : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+ 
+	
+}
