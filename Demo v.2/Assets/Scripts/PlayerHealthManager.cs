b6b33f0f390eb4f1dae0260a297e1a9bac843aad
ThatZombieAlien <sanna.gustafsson0706@gmail.com>
2016-04-14 13:48:04 +0200

    La till health gain
    Spelaren återfår nu sin hp baserat på en timer

--- Demo v.2/Assets/Scripts/PlayerHealthManager.cs
+++ Demo v.2/Assets/Scripts/PlayerHealthManager.cs
@@ -5,6 +5,8 @@ public class PlayerHealthManager : MonoBehaviour
 {
     public int playerMaxHealth;
     public int playerCurrentHealth;
+    private float regenTimer;
+    public float regenTimerMax;
 
     void Start()
     {
@@ -12,11 +14,20 @@ public class PlayerHealthManager : MonoBehaviour
     }
 
     void Update()
-    {
+    {        
         if (playerCurrentHealth <= 0)
         {
             gameObject.SetActive(false);
         }
+
+        regenTimer -= Time.deltaTime;
+        if (regenTimer <= 0 && playerCurrentHealth < playerMaxHealth)
+            {
+                playerCurrentHealth += 5;
+                regenTimer = regenTimerMax;
+                // spelaren får +5 hälsa baserat på en timer, dock endast om hälsan är lägre än max hälsan
+            }
+
     }
 
     public void HurtPlayer(int damageToGive)
