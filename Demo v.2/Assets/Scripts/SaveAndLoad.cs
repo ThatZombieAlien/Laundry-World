b086a7a2eaa07fb2a52c04686e3a9f431cd4bb43
Tzatu <hamnbytare@gmail.com>
2016-04-21 10:40:27 +0200

    skrev lite kommentarer

---
+++ Demo v.2/Assets/Scripts/SaveAndLoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveAndLoad : MonoBehaviour
+{
+
+    public void SavePosition() //sparar positionerna
+    {
+        PlayerPrefs.SetFloat("PlayerX", transform.position.x); //sparar spelarens x-position
+        PlayerPrefs.SetFloat("PlayerY", transform.position.y); //sparar spelarens y-position
+
+    }
+
+    public void LoadPosition()
+    {
+        float x = PlayerPrefs.GetFloat("PlayerX"); //hämtar spelarens x-position
+        float y = PlayerPrefs.GetFloat("PlayerY"); //hämtar spelarens y-position
+
+        transform.position = new Vector3(x, y); //sätter spelarens pos till den sparade posen.
+    }	
+}
+
