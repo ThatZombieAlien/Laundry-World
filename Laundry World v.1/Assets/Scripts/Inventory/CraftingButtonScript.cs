2dbab7bf265ec9da2226d4b825f2951aa91e5207
annaliljaenglund <annaliljaenglund@gmail.com>
2016-04-26 16:07:22 +0200

    Revert "Revert "Crafting - symbol""
    This reverts commit fc7de260c628d0856bbaebb751c7736929b30f7c.

---
+++ Laundry World v.1/Assets/Scripts/Inventory/CraftingButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraftingButtonScript : MonoBehaviour 
+{
+    public void OnCraftingClick()
+    {
+        Debug.Log("crafting");
+    }
+}
