6d397969ecfb1d43abf98ba67a2594ade66f5e4a
annaliljaenglund <annaliljaenglund@gmail.com>
2016-04-29 11:18:54 +0200

    Character panel - fix 2

---
+++ Laundry World v.1/Assets/Scripts/Inventory/EquipmentSlot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
+
+public class EquipmentSlot : MonoBehaviour
+{
+    public string equipmentType;
+    public Item equippedItem = null;
+}
--- Laundry World v.1/Assets/Scripts/Items/ItemData/ItemDatabase.cs
+++ Laundry World v.1/Assets/Scripts/Items/ItemData/ItemDatabase.cs
@@ -8,7 +8,7 @@ public class ItemDatabase : MonoBehaviour
 {
     private List<Item> database = new List<Item>();
     private JsonData itemData;
-    
+
     void Start()
     {
         itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
@@ -17,7 +17,7 @@ public class ItemDatabase : MonoBehaviour
 
     public Item FetchItemByID(int id)
     {
-        for(int i = 0; i < itemData.Count; i++)
+        for (int i = 0; i < itemData.Count; i++)
         {
             if (database[i].ID == id)
             {
@@ -29,16 +29,17 @@ public class ItemDatabase : MonoBehaviour
 
     void ConstructItemDatabase()
     {
-        for(int i = 0; i < itemData.Count; i++)
+        for (int i = 0; i < itemData.Count; i++)
         {
-            database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"],
+            database.Add(new Item(itemData[i]["type"].ToString(), (int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"],
                 (bool)itemData[i]["stackable"], itemData[i]["slug"].ToString(), (bool)itemData[i]["consumable"]));
         }
-    } 
+    }
 }
 
 public class Item
 {
+    public string Type { get; set; }
     public int ID { get; set; }
     public string Title { get; set; }
     public int Value { get; set; }
@@ -47,8 +48,9 @@ public class Item
     public bool Consumable { get; set; }
     public Sprite Sprite { get; set; }
 
-    public Item(int id, string title, int value, bool stackable, string slug, bool consumable)
+    public Item(string type, int id, string title, int value, bool stackable, string slug, bool consumable)
     {
+        this.Type = type;
         this.ID = id;
         this.Title = title;
         this.Value = value;
@@ -57,7 +59,7 @@ public class Item
         this.Consumable = consumable;
         this.Sprite = Resources.Load<Sprite>(slug);
     }
-    
+
 
     //Empty slot
     public Item()
