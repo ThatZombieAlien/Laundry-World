664bd7621706f0b63cf87cfc9ac9d14a0b9ee36c
ThatZombieAlien <sanna.gustafsson0706@gmail.com>
2016-05-04 21:50:11 +0200

    Musik
    Lade till musik
    Försökte fixa en av/på knapp för musiken, men fick det inte att fungera
    (någon annan får gärna kolla på det om ni har tid)

---
+++ Laundry World v.1/Assets/Scripts/Music/Music.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+ //   public AudioSource mainTheme;
+ //   public static bool playMusic = true;
+
+ //   void Start ()
+ //   {
+ //       if (playMusic)
+ //       {
+ //           mainTheme.Play();
+ //       }
+ //   }
+	//void Update()
+ //   {
+ //       if (playMusic)
+ //       {
+ //           mainTheme.Play();
+ //       }
+ //       else
+ //       {
+ //           mainTheme.Stop();
+ //       }
+ //   }
+
+
+}
---
+++ Laundry World v.1/Assets/Scripts/Music/MusicOnOff.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicOnOff : MonoBehaviour {
+
+
+    void Start()
+    {
+    }
+    public void Click()
+    {
+        //if (!Music.playMusic)
+        //{
+        //    Music.playMusic = true;
+        //}
+        //else
+        //{
+        //    Music.playMusic = false;
+        //}
+    }
+
+}
