c7035545877e9f8bd409cbdeb2747ccd33c15ddc
Tzatu <hamnbytare@gmail.com>
2016-05-12 18:43:33 +0200

    Lagade pausemenyn

---
+++ Laundry World v.1/Assets/Scripts/Pause.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject pauseButton, pausePanel;
+	// Use this for initialization
+
+	public void Start()
+	{
+		OnUnpause ();
+	}
+	public void Onpause()
+	{
+		pausePanel.SetActive (true);
+		pauseButton.SetActive (false);
+		Time.timeScale = 0;
+
+	}
+
+	public void OnUnpause()
+	{
+		pausePanel.SetActive (false);
+		pauseButton.SetActive (true);
+		Time.timeScale = 1;
+	}
+
+
+
+}
+
+
