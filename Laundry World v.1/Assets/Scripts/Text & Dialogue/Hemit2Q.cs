6f61e183da0060bcecbbe72f888d25493b3c072e
Tzatu <hamnbytare@gmail.com>
2016-05-19 10:51:12 +0200

    Questscript-buggigt
    Dialogrutan försvinner uppåt, och dialogen försvinner vid vissa alt. nån
    får gärna kolla på det.

---
+++ Laundry World v.1/Assets/Scripts/Text & Dialogue/Hemit2Q.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hemit2Q : MonoBehaviour
+{
+    public Hermit2Quest dialogue;
+    //private PlayerStats playerStats;
+
+    void Start()
+    {
+        //dialogue = GameObject.Find("NPC(1)").GetComponent<QuestDialogue>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Spelaren plockar upp ett föremål, som sedan förstörs, detta sätter questet till gjort - och det kan nu rapporteras in
+        if (dialogue.activateQuest)
+        {
+            if (col.tag == "Player")
+            {
+                Destroy(gameObject);
+                dialogue.hasDoneQuest = true;
+            }
+        }
+
+    }
+}
--- Laundry World v.1/Assets/Scripts/Text & Dialogue/Hermit2Quest.cs
+++ Laundry World v.1/Assets/Scripts/Text & Dialogue/Hermit2Quest.cs
@@ -1,241 +1,332 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Hermit2Quest : MonoBehaviour {
+public class Hermit2Quest : MonoBehaviour
+{
 
 
-	// OBS: Endast exempel på struktur
-	public string[] lines;
-	public string[] answerButtons;
+    // OBS: Endast exempel på struktur
+    public string[] lines;
+    public string[] answerButtons;
 
-	public bool destoryOnFinish;
-	bool displayDialogue = false;
-	public bool activateQuest = false;
-	public bool hasDoneQuest = false;
+    public bool destoryOnFinish;
+    bool displayDialogue = false;
+    public bool activateQuest = false;
+    public bool hasDoneQuest = false;
 
-	bool line0 = true;
-	bool line1 = false;
-	bool line2 = false;
-	bool line3 = false;
-	bool line9 = false;
-	bool line10 = false;
-
-
-	bool finishedDialogue = false;
-	public int reward;
-	public TextBoxManager textManager;
-	public PlayerController player;
-	public NPCController npc;
-	private GUIStyle guiStyle = new GUIStyle();
-
-	private PlayerStats playerStats;
+    bool line0 = true;
+    bool line1 = false;
+    bool line2 = false;
+    bool line3 = false;
+    bool line4 = false;
+    bool line5 = false;
+    bool line6 = false;
+    bool line7 = false;
+    bool line8 = false;
+    bool line9 = false;
+    bool line10 = false;
+    bool line13 = false;
 
 
-
-	void Start()
-	{
-		//textManager = FindObjectOfType<TextBoxManager>();
-		playerStats = FindObjectOfType<PlayerStats>();
-	}
-
-	void Update()
-	{
-		if (displayDialogue && !activateQuest && !hasDoneQuest)
-		{
-			//textManager.EnableTextBox();
-			player.canMove = false;
-			npc.canMove = false;
-		}
-		if (!displayDialogue)
-		{
-			//textManager.DisableTextBox();
-			player.canMove = true;
-			npc.canMove = true;
-		}
-		if (displayDialogue && hasDoneQuest)
-		{
-			//textManager.EnableTextBox();
-			player.canMove = false;
-			npc.canMove = false;
-		}
-
-	}
-
-	void OnGUI()
-	{
-
-		GUILayout.BeginArea(new Rect(Screen.width / 2 - 150, Screen.height - 100, 350, 600));
-
-		guiStyle.fontSize = 16; // ändra storlek
-		guiStyle.normal.textColor = Color.white; // ändra färg
-
-		if (displayDialogue)
-//			&& !activateQuest && line1)
-		{
-			if (activateQuest)
-			{
-				GUILayout.Label(lines[12], guiStyle);
-
-				if (GUILayout.Button(answerButtons[7]))
-				{
-					displayDialogue = false;
-				}
-			}
-
-			if (!activateQuest)
-			{
-				if (line0) 
-				{
-					GUILayout.Label (lines [0], guiStyle);
-
-					if (GUILayout.Button (answerButtons [0])) 
-					{
-						line1 = true;
-						line0 = false;
-					}
-					if (GUILayout.Button (answerButtons [7])) 
-					{
-						displayDialogue = false;
-
-					}
-				}
-
-				if (line1)
-				{
-			GUILayout.Label(lines[1], guiStyle);
-
-			if (GUILayout.Button(answerButtons[1]))
-			{
-//				activateQuest = true;
-				line1 = false;
-				line2 = true;
-			}
-
-			if (GUILayout.Button(answerButtons[3]))
-			{
-				line1 = false;
-				line9 = true;
-			
-
-			}
-		}
-
-				if (line2)
-				{
-					GUILayout.Label(lines[2], guiStyle);
-
-					if (GUILayout.Button(answerButtons[2]))
-					{
-						//				activateQuest = true;
-						line2 = false;
-						line3 = true;
-					}
-
-					if (GUILayout.Button(answerButtons[4]))
-					{
-						line2 = false;
-						line9 = true;
-
-					}
-				}
-
-				if (line9)
-				{
-					GUILayout.Label(lines[9], guiStyle);
-					GUILayout.Label(lines[10], guiStyle);
+    bool finishedDialogue = false;
+    bool talkedAboutHouse = false;
+    bool startedTalking = false;
 
 
-					if (GUILayout.Button(answerButtons[2]))
-					{
-						//				activateQuest = true;
-						line2 = false;
-						line3 = true;
-					}
+    public int reward;
+    public TextBoxManager textManager;
+    public PlayerController player;
+    public NPCController npc;
+    private GUIStyle guiStyle = new GUIStyle();
 
-					if (GUILayout.Button(answerButtons[4]))
-					{
-						line2 = false;
-						line9 = true;
+    private PlayerStats playerStats;
 
-					}
-				}
 
-		if (activateQuest && hasDoneQuest && displayDialogue)
-		{
-			GUILayout.Label(lines[2], guiStyle);
 
-			if (GUILayout.Button(answerButtons[2]))
-			{
-				displayDialogue = false;
-				line3 = true;
-				line1 = false;
-				activateQuest = false;
-				PlayerPurse.playerGold += reward;
+    void Start()
+    {
+        //textManager = FindObjectOfType<TextBoxManager>();
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
 
-				playerStats.AddExperience(20);
-			}
+    void Update()
+    {
+        if (displayDialogue && !activateQuest && !hasDoneQuest)
+        {
+            //textManager.EnableTextBox();
+            player.canMove = false;
+            npc.canMove = false;
+        }
+        if (!displayDialogue)
+        {
+            //textManager.DisableTextBox();
+            player.canMove = true;
+            npc.canMove = true;
+        }
+        if (displayDialogue && hasDoneQuest)
+        {
+            //textManager.EnableTextBox();
+            player.canMove = false;
+            npc.canMove = false;
+        }
 
-			if (GUILayout.Button(answerButtons[3]))
-			{
-				displayDialogue = false;
-			}
-		}
+    }
 
-		if (line3)
-		{
-			GUILayout.Label(lines[3], guiStyle);
+    void OnGUI()
+    {
 
-			if (GUILayout.Button(answerButtons[0]))
-			{
-				displayDialogue = false;;
-				line3 = false;
-			}
-		}
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 150, Screen.height - 100, 350, 600));
 
-		GUILayout.EndArea();
+        guiStyle.fontSize = 16; // ändra storlek
+        guiStyle.normal.textColor = Color.white; // ändra färg
 
-		if (activateQuest) // ritar ut meddelande om pågående quest
-		{
-			//GUI.DrawTexture(new Rect(10, 10, 200, 150), texture1);
-			GUILayout.BeginArea(new Rect(Screen.width - 300, Screen.height * 0.2f, 250, 250)); // "putta ner quests beroende på hur många man har?
+        if (displayDialogue && !activateQuest && line0)        //            && !activateQuest && line1)
+        {
+            GUILayout.Label(lines[0], guiStyle);
 
-			GUILayout.Box("New Quest: Find the book"); // sätt in bools för vilken text som ska visas beroende på vilket quest man är på?
+            //            startedTalking = true;
 
-			GUILayout.EndArea();
-		}
+            if (GUILayout.Button(answerButtons[0]))
+            {
+                line1 = true;
+                displayDialogue = true;
+                line0 = false;
+            }
 
-	}
-	}
-	}
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.gameObject.name == "Player")
-		{
-			//if (Input.GetKeyDown(KeyCode.E))
-			{
-				if (!activateQuest && !hasDoneQuest)
-				{
-					line1 = true;
-				}
+            if (GUILayout.Button(answerButtons[7]))
+            {
+                displayDialogue = false;
+            }
 
-				displayDialogue = true;
-				Debug.Log("An object Collided");
-			}
-		}
-	}
+        }
 
-	void OnTriggerExit2D(Collider2D other)
-	{
-		if (other.gameObject.name == "Player")
-		{
-			displayDialogue = false;
-		}
-	}
+        if (line1 && displayDialogue)
+        {
+            if (line0)
+            {
+                GUILayout.Label(lines[0], guiStyle);
 
-	void QuestCompleted()
-	{
-		// lägger till reward (om questet har det) till spelarens "pengar"
-		PlayerPurse.playerGold += reward;
-	}
+                if (GUILayout.Button(answerButtons[0]))
+                {
+                    line1 = true;
+                    line0 = false;
+                }
+                if (GUILayout.Button(answerButtons[7]))
+                {
+                    displayDialogue = false;
+
+                }
+            }
+
+            if (line1)
+            {
+                GUILayout.Label(lines[1], guiStyle);
+
+                if (GUILayout.Button(answerButtons[1]))
+                {
+                    line1 = false;
+                    line2 = true;
+                }
+
+                if (GUILayout.Button(answerButtons[3]))
+                {
+                    line1 = false;
+                    line9 = true;
+
+
+                }
+            }
+
+            if (line2)
+            {
+                GUILayout.Label(lines[2], guiStyle);
+
+                if (GUILayout.Button(answerButtons[2]))
+                {
+                    line2 = false;
+                    line3 = true;
+                }
+
+                if (GUILayout.Button(answerButtons[4]))
+                {
+                    line2 = false;
+                    line5 = true;
+
+                }
+            }
+            if (line3)
+            {
+                GUILayout.Label(lines[3], guiStyle);
+
+                if (GUILayout.Button(answerButtons[4]))
+                {
+                    line5 = true;
+                    line3 = false;
+                }
+
+                //                    if (GUILayout.Button(answerButtons[7]))
+                //                    {
+                //                        line3 = false;
+                //                        displayDialogue = false;
+                ////                        talkedAboutHouse = true;   //sätta på den gör att man inta kan prata igen efter avbruten dialog
+                //
+                //                    }
+            }
+
+            if (line9)
+            {
+                GUILayout.Label(lines[9], guiStyle);
+                GUILayout.Label(lines[10], guiStyle);
+
+
+                if (GUILayout.Button(answerButtons[2]))
+                {
+                    line9 = false;
+                    line3 = true;
+                }
+
+                if (GUILayout.Button(answerButtons[4]))
+                {
+                    line5 = true;
+                    line9 = false;
+
+                }
+            }
+
+
+            if (line5)
+            {
+                GUILayout.Label(lines[5], guiStyle);
+                GUILayout.Label(lines[6], guiStyle);
+
+
+                if (GUILayout.Button(answerButtons[5]))
+                {
+                    line5 = false;
+                    line7 = true;
+                }
+
+            }
+
+
+            if (line7)
+            {
+                GUILayout.Label(lines[7], guiStyle);
+
+                if (GUILayout.Button(answerButtons[6]))
+                {
+                    activateQuest = true;
+                    line7 = false;
+                    line8 = true;
+                }
+
+            }
+
+            if (line8)
+            {
+                GUILayout.Label(lines[8], guiStyle);
+
+                if (GUILayout.Button(answerButtons[7]))
+                {
+                    line8 = false;
+
+                    displayDialogue = false;
+                }
+
+            }
+
+
+
+            if (activateQuest && hasDoneQuest && displayDialogue)
+            {
+                GUILayout.Label(lines[13], guiStyle);
+
+                if (GUILayout.Button(answerButtons[2]))
+                {
+                    displayDialogue = false;
+                    //                line13 = true;
+                    line13 = false;
+                    activateQuest = false;
+                    PlayerPurse.playerGold += reward;
+
+                    playerStats.AddExperience(20);
+                }
+
+                if (GUILayout.Button(answerButtons[3]))
+                {
+                    displayDialogue = false;
+                }
+            }
+
+            //        if (line3)
+            //        {
+            //            GUILayout.Label(lines[3], guiStyle);
+            //
+            //            if (GUILayout.Button(answerButtons[0]))
+            //            {
+            //                displayDialogue = false;;
+            //                line3 = false;
+            //            }
+            //        }
+
+            GUILayout.EndArea();
+
+            if (activateQuest) // ritar ut meddelande om pågående quest
+            {
+
+                //GUI.DrawTexture(new Rect(10, 10, 200, 150), texture1);
+                GUILayout.BeginArea(new Rect(Screen.width - 300, Screen.height * 0.2f, 250, 250)); // "putta ner quests beroende på hur många man har?
+
+                GUILayout.Box("New Quest: Find building material"); // sätt in bools för vilken text som ska visas beroende på vilket quest man är på?
+
+                GUILayout.EndArea();
+            }
+
+
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            //if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!activateQuest && !hasDoneQuest)
+                {
+                    line0 = true;
+                }
+                //
+                //                if (!activateQuest && !hasDoneQuest)  //&& talkedAboutHouse && startedTalking
+                //                {
+                //                    Debug.Log("blurb");
+                //
+                //                    line3 = true;
+                //                
+                //                }
+
+                displayDialogue = true;
+                //Debug.Log("An object Collided");
+            }
+
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            displayDialogue = false;
+        }
+    }
+
+    void QuestCompleted()
+    {
+        // lägger till reward (om questet har det) till spelarens "pengar"
+        PlayerPurse.playerGold += reward;
+    }
+
 }
+
 
