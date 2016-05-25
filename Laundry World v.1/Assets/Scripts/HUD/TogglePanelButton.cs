35eac285b4b76aff92c0a67c7557e92dd2357333
Tzatu <hamnbytare@gmail.com>
2016-05-25 18:10:35 +0200

    Fixade smågrejer

---
+++ Laundry World v.1/Assets/Scripts/HUD/TogglePanelButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+//gjord av Maria
+
+public class TogglePanelButton : MonoBehaviour 
+{
+	
+	public void TogglePanel (GameObject panel)
+	{
+		panel.SetActive (!panel.activeSelf); //aktiverar/avaktiverar en panel
+	}
+
+
+}
