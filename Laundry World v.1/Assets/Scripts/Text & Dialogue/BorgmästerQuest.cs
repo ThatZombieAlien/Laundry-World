f05c8a3b7b47c74f5e89a58e60cbd81467e4f5a0
ThatZombieAlien <sanna.gustafsson0706@gmail.com>
2016-05-25 12:09:19 +0200

    Lagt till quest-script

---
+++ Laundry World v.1/Assets/Scripts/Text & Dialogue/BorgmästerQuest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BorgmästerQuest : MonoBehaviour
+{
+
+    public BorgmästerQuestDialog dialog;
+    //public GameObject detergentBorgmästarQuest;
+    //private PlayerStats playerStats;
+
+    void Start()
+    {
+        //dialogue = GameObject.Find("NPC(1)").GetComponent<QuestDialogue>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Spelaren plockar upp ett föremål, som sedan förstörs, detta sätter questet till gjort - och det kan nu rapporteras in
+        if (dialog.activateQuest)
+        {
+            //Destroy(detergentBorgmästarQuest);
+
+            if (collider.tag == "Player")
+            {
+                Destroy(gameObject);
+                dialog.hasDoneQuest = true;
+
+            }
+        }
+
+    }
+}
---
+++ Laundry World v.1/Assets/Scripts/Text & Dialogue/BorgmästerQuestDialog.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BorgmästerQuestDialog : MonoBehaviour {
+
+
+    public string[] lines;
+    public string[] answerButtons;
+
+    //public bool destoryOnFinish;
+    bool displayDialogue = false;
+    public bool activateQuest = false;
+    public bool hasDoneQuest = false;
+    bool line2 = false;
+    bool line1 = true;
+    bool line3 = false;
+    bool line4 = false;
+    //bool finishedDialogue = false;
+    public int reward;
+    public TextBoxManager textManager;
+    public PlayerController player;
+    public NPCController npc;
+    private GUIStyle guiStyle = new GUIStyle();
+
+    private PlayerStats playerStats;
+    //private PlayerPurse playerReward;
+
+
+
+    void Start()
+    {
+        //textManager = FindObjectOfType<TextBoxManager>();
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    void Update()
+    {
+        if (displayDialogue && !activateQuest && !hasDoneQuest)
+        {
+            //textManager.EnableTextBox();
+            player.canMove = false;
+            npc.canMove = false;
+        }
+        if (!displayDialogue)
+        {
+            //textManager.DisableTextBox();
+            player.canMove = true;
+            npc.canMove = true;
+        }
+        if (displayDialogue && hasDoneQuest)
+        {
+            //textManager.EnableTextBox();
+            player.canMove = false;
+            npc.canMove = false;
+        }
+
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 150, Screen.height - 100, 350, 600));
+
+        guiStyle.fontSize = 16; // ändra storlek
+        guiStyle.normal.textColor = Color.white; // ändra färg
+
+        if (displayDialogue && !activateQuest && line1)  ///Första dialogen
+        {
+
+            GUILayout.Label(lines[0], guiStyle);
+
+            if (GUILayout.Button(answerButtons[0]))
+            {
+                line2 = true;
+                displayDialogue = true;
+                line1 = false;
+            }
+
+            if (GUILayout.Button(answerButtons[1]))
+            {
+                displayDialogue = false;
+            }
+        }
+
+        if (line2 && displayDialogue)   ///Andra dialogen
+        {
+            displayDialogue = true;
+            GUILayout.Label(lines[1], guiStyle);
+
+            if (GUILayout.Button(answerButtons[2]))
+            {
+                displayDialogue = true;
+                line3 = true;
+                line2 = false;
+            }
+
+            if (GUILayout.Button(answerButtons[1]))
+            {
+                displayDialogue = false;
+                line2 = false;
+            }
+        }
+
+        if (line3 && displayDialogue)   //Tredje dialogen
+        {
+            displayDialogue = true;
+            GUILayout.Label(lines[2], guiStyle);
+
+            if (GUILayout.Button(answerButtons[3]))
+            {
+                activateQuest = true;
+                displayDialogue = false;
+                line4 = true;
+                line3 = false;
+            }
+
+            if (GUILayout.Button(answerButtons[1]))
+            {
+                displayDialogue = false;
+                line3 = false;
+            }
+        }
+
+        if (activateQuest && hasDoneQuest && displayDialogue)
+        {
+            GUILayout.Label(lines[3], guiStyle);
+
+            if (GUILayout.Button(answerButtons[4]))
+            {
+                displayDialogue = false;
+                line1 = false;
+                activateQuest = false;
+                PlayerPurse.playerGold += 150;
+
+                playerStats.AddExperience(20);
+               
+            }
+
+        }
+
+
+
+        GUILayout.EndArea();
+
+        if (activateQuest) // ritar ut meddelande om pågående quest
+        {
+            //GUI.DrawTexture(new Rect(10, 10, 200, 150), texture1);
+            GUILayout.BeginArea(new Rect(Screen.width - 350, Screen.height * 0.2f, 350, 350)); // "putta ner quests beroende på hur många man har?
+
+            GUILayout.Box("New Quest:Find detergent and clean the well"); // sätt in bools för vilken text som ska visas beroende på vilket quest man är på?
+
+            GUILayout.EndArea();
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            //if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!activateQuest && !hasDoneQuest)
+                {
+                    line1 = true;
+                }
+
+                displayDialogue = true;
+                Debug.Log("An object Collided");
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            displayDialogue = false;
+        }
+    }
+
+    void QuestCompleted()
+    {
+        // lägger till reward (om questet har det) till spelarens "pengar"
+        PlayerPurse.playerGold += reward;
+    }
+}
